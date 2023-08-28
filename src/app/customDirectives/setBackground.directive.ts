92564acc630f1549f0301c582e31b1a6626e0428
Masihdev <masihullahmassudi85@gmail.com>
2023-08-28 10:55:52 +0200

    property binding in directives

--- src/app/customDirectives/setBackground.directive.ts
+++ src/app/customDirectives/setBackground.directive.ts
@@ -1,10 +1,12 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from "@angular/core";
+import { Directive, ElementRef, OnInit, Renderer2, Input } from "@angular/core";
 
 @Directive({
     selector: '[setBackground]'
 })
 export class SetBackground implements OnInit {
     // private element: ElementRef;
+    @Input() backColor: string = '#36454f';
+    @Input() textColor: string = 'white';
 
     constructor(private element: ElementRef, private renderer: Renderer2) {
         // this.element = element;
@@ -14,8 +16,8 @@ export class SetBackground implements OnInit {
     ngOnInit(): void {
         // this.element.nativeElement.style.backgroundColor = '#36454f';
         // this.element.nativeElement.style.color = 'white';
-        this.renderer.setStyle(this.element.nativeElement, 'background', '#36454f');
-        this.renderer.setStyle(this.element.nativeElement, 'color', 'white');
+        this.renderer.setStyle(this.element.nativeElement, 'background', this.backColor);
+        this.renderer.setStyle(this.element.nativeElement, 'color', this.textColor);
         this.renderer.setAttribute(this.element.nativeElement, 'title', 'This is an example.')
     }
 }
